1db5c862a63576f9e2ccd94db9580969da0f5f13
madhurmoms <madhurmoms@gmail.com>
2023-06-06 17:34:54 +0530

    send employee to next page

--- Front End/src/components/Student.js
+++ Front End/src/components/Student.js
@@ -1,10 +1,11 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import Form from 'react-bootstrap/Form';
 import axios from 'axios';
 
 function Student() {
   const [name, setName] = useState('');
   const [address, setAddress] = useState('');
+  const [students, setStudents] = useState([]);
 
   const handleClick = (e) => {
     e.preventDefault();
@@ -40,27 +41,66 @@ function Student() {
       });
   };
 
+  useEffect(() => {
+    // Fetch student data from the backend when the component mounts
+    fetchStudentData();
+  }, []);
+
+  const fetchStudentData = () => {
+    axios
+      .get('http://localhost:8080/student/getall')
+      .then((response) => {
+        setStudents(response.data);
+      })
+      .catch((error) => {
+        console.error(error);
+        alert('Failed to fetch student data');
+      });
+  };
+
   return (
-    <Form>
-      <h3>Student Name</h3>
-      <input
-        type="text"
-        placeholder="Enter Name"
-        value={name}
-        onChange={(e) => setName(e.target.value)}
-      />
-      <h3>Student Address</h3>
-      <input
-        type="text"
-        placeholder="Enter Address"
-        value={address}
-        onChange={(e) => setAddress(e.target.value)}
-      />
-      <br /> <br />
-      <button type="submit" onClick={handleClick}>
-        Submit
-      </button>
-    </Form>
+    <div>
+      <Form>
+        <h3>Student Name</h3>
+        <input
+          type="text"
+          placeholder="Enter Name"
+          value={name}
+          onChange={(e) => setName(e.target.value)}
+        />
+        <h3>Student Address</h3>
+        <input
+          type="text"
+          placeholder="Enter Address"
+          value={address}
+          onChange={(e) => setAddress(e.target.value)}
+        />
+        <br /> <br />
+        <button type="submit" onClick={handleClick}>
+          Submit
+        </button>
+      </Form>
+
+      <div style={{ marginLeft: '570px', marginTop: '50px', marginBottom: '50px' }}>
+        <table border='1px solid black'>
+          <thead>
+            <tr>
+              <th>Name</th>
+              <th>Address</th>
+            </tr>
+          </thead>
+          <tbody>
+            {students.map((student, index) => (
+              <tr key={index}>
+                <td>{student.name}</td>
+                <td>{student.address}</td>
+              </tr>
+            ))}
+          </tbody>
+
+        </table>
+      </div>
+    </div>
   );
 }
 
